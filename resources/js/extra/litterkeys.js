6dfb9d0adf108fb1a857b9c24f5ba5a707a9dc37
xlcrr <info@openlittermap.com>
2020-08-29 12:58:44 +0100

    update litterkeys

--- resources/js/extra/litterkeys.js
+++ resources/js/extra/litterkeys.js
@@ -1,7 +1,6 @@
 // Here we create the reverse of column_name -> Frontend string to display
 // Currently, the extract string is needed to verify the data
 export const litterkeys = {
-	// Smoking
     smoking: [
         { id: 0, key: 'butts' },            // Cigarette/Butts
         { id: 1, key: 'lighters' },         // Lighters
@@ -15,188 +14,198 @@ export const litterkeys = {
         { id: 9, key: 'vape_pen' },         // Vape pen
         { id: 10, key: 'vape_oil' },        // Vape oil
     ],
-	// Alcohol
-	beerBottle: 'Beer Bottles',
-	spiritBottle: 'Spirit Bottles',
-	wineBottle: 'Wine Bottles',
-	beerCan: 'Beer Cans',
-	brokenGlass: 'Broken Glass',
-	bottleTops: 'Beer bottle tops',
-	paperCardAlcoholPackaging: 'Paper Packaging',
-	plasticAlcoholPackaging: 'Plastic Packaging',
-	alcoholOther: 'Alcohol-Other',
-	pint: 'Pint Glass',
-	six_pack_rings: 'Six-pack rings',
-	alcohol_plastic_cups: 'Plastic Cups',
-	// Coffee
-	coffeeCups: 'Coffee Cups',
-	coffeeLids: 'Coffee Lids',
-	coffeeOther: 'Coffee-Other',
-	// Food
-	sweetWrappers: 'Sweet Wrappers',
-	paperFoodPackaging: 'Paper/Cardboard Packaging',
-	plasticFoodPackaging: 'Plastic Packaging',
-	plasticCutlery: 'Plastic Cutlery',
-	crisp_small: 'Crisp/Chip Packet (small)',
-	crisp_large: 'Crisp/Chip Packet (large)',
-	styrofoam_plate: 'Styrofoam Plate',
-	napkins: 'Napkins',
-	sauce_packet: 'Sauce Packet',
-	glass_jar: 'Glass Jar',
-	glass_jar_lid: 'Glass Jar Lid',
-	foodOther: 'Food-other',
-	// SoftDrinks
-	waterBottle: 'Plastic Water bottle',
-	fizzyDrinkBottle: 'Plastic Fizzy Drink bottle',
-	tinCan: 'Can',
-	bottleLid: 'Bottle Tops',
-	bottleLabel: 'Bottle Labels',
-	sportsDrink: 'Sports Drink bottle',
-	straws: 'Straws',
-	plastic_cups: 'Plastic Cups',
-	plastic_cup_tops: 'Plastic Cup Tops',
-	milk_bottle: 'Milk Bottle',
-	milk_carton: 'Milk Carton',
-	paper_cups: 'Paper Cups',
-	juice_cartons: 'Juice Cartons',
-	juice_bottles: 'Juice Bottles',
-	juice_packet: 'Juice Packet',
-	ice_tea_bottles: 'Ice Tea Bottles',
-	ice_tea_can: 'Ice Tea Can',
-	energy_can: 'Energy Can',
-	pullring: 'Pull-ring',
-	strawpacket: 'Straw Packaging',
-	styro_cup: 'Styrofoam Cup',
-	softDrinkOther: 'Soft Drink (other)',
-	// Sanitary
-	gloves: 'Gloves',
-	facemask: 'Facemask',
-	condoms: 'Condoms',
-	nappies: 'Nappies',
-	menstral: 'Menstral',
-	deodorant: 'Deodorant',
-	ear_swabs: 'Ear Swabs',
-	tooth_pick: 'Tooth Pick',
-	tooth_brush: 'Tooth Brush',
-	wetwipes: 'Wet Wipes',
-	sanitaryOther: 'Sanitary (other)',
-	// Other
-	dogshit: 'Pet Surprise',
-	pooinbag: 'Surprise In Bag',
-	automobile: 'Automobile',
-	clothing: 'Clothing',
-	traffic_cone: 'Traffic cone',
-	life_buoy: 'Life Buoy',
-	plastic: 'Unidentified Plastic',
-	dump: 'Illegal Dumping',
-	metal: 'Metal Object',
-	plastic_bags: 'Plastic Bags',
-	election_posters: 'Election Posters',
-	forsale_posters: 'For Sale Posters',
-	books: 'Books',
-	magazine: 'Magazines',
-	paper: 'Paper',
-	stationary: 'Stationery',
-	washing_up: 'Washing-up Bottle',
-	hair_tie: 'Hair Tie',
-	ear_plugs: 'Ear Plugs (music)',
-	batteries: 'Batteries',
-	elec_small: 'Electric small',
-	elec_large: 'Electric large',
-	other: 'Other (other)',
-	random_litter: 'Random Litter',
-	// Dumping
-	small: 'Small',
-	medium: 'Medium',
-	large: 'Large',
-	// Industrial
-	oil: 'Oil',
-	industrial_plastic: 'Plastic',
-	chemical: 'Chemical',
-	bricks: 'Bricks',
-	tape: 'Tape',
-	industrial_other: 'Other',
-
-	// Coastal
-	microplastics: 'Microplastics',
-	mediumplastics: 'Mediumplastics',
-	macroplastics: 'Macroplastics',
-	rope_small: 'Rope small',
-	rope_medium: 'Rope medium',
-	rope_large: 'Rope large',
-	fishing_gear_nets: 'Fishing gear/nets',
-	buoys: 'Buoys',
-	degraded_plasticbottle: 'Degraded Plastic Bottle',
-	degraded_plasticbag: 'Degraded Plastic Bag',
-	degraded_straws: 'Degraded Drinking Straws',
-	degraded_lighters: 'Degraded Lighters',
-	balloons: 'Balloons',
-	lego: 'Lego',
-	shotgun_cartridges: 'Shotgun Cartridges',
-	styro_small: 'Styrofoam small',
-	styro_medium: 'Styrofoam medium',
-	styro_large: 'Styrofoam large',
-	coastal_other: 'Coastal (other)',
-	// Brands
-	adidas: 'Adidas',
-	amazon: 'Amazon',
-	apple: 'Apple',
-	budweiser: 'Budweiser',
-	camel: 'Camel',
-	coke: 'Coca-Cola',
-	colgate: 'Colgate',
-	corona: 'Corona',
-	costa: 'Costa',
-	doritos: 'Doritos',
-	drpepper: 'DrPepper',
-	dunnes: 'Dunnes',
-	duracell: 'Duracell',
-	durex: 'Durex',
-	esquires: 'Esquires',
-	frank_and_honest: 'Frank-and-Honest',
-	fritolay: 'Frito-Lay',
-	gatorade: 'Gatorade',
-	gillette: 'Gillette',
-	haribo: 'Haribo',
-	heineken: 'Heineken',
-	insomnia: 'Insomnia',
-	kellogs: 'Kellogs',
-	kfc: 'KFC',
-	// lego: 'Lego', -> duplicated by Coastal
-	lidl: 'Lidl',
-	lindenvillage: 'Lindenvillage',
-	lolly_and_cookes: 'Lolly-and-cookes',
-	loreal: 'Loreal',
-	lucozade: 'Lucozade',
-	nescafe: 'Nescafe',
-	nestle: 'Nestle',
-	marlboro: 'Marlboro',
-	mcdonalds: 'McDonalds',
-	nike: 'Nike',
-	obriens: 'O-Briens',
-	pepsi: 'Pepsi',
-	powerade: 'Powerade',
-	redbull: 'Redbull',
-	ribena: 'Ribena',
-	samsung: 'Samsung',
-	sainsburys: 'Sainsburys',
-	spar: 'Spar',
-	stella: 'Stella',
-	subway: 'Subway',
-	supermacs: 'Supermacs',
-	supervalu: 'Supervalu',
-	starbucks: 'Starbucks',
-	tayto: 'Tayto',
-	tesco: 'Tesco',
-	thins: 'Thins',
-	volvic: 'Volvic',
-	waitrose: 'Waitrose',
-	walkers: 'Walkers',
-	woolworths: 'Woolworths',
-	wilde_and_greene: 'Wilde-and-Greene',
-	wrigleys: 'Wrigleys',
-	// Trashdog
-	trashdog: 'TrashDog',
-	littercat: 'LitterCat',
-	duck: 'LitterDuck'
+    alcohol: [
+        { id: 0, key: 'beerBottle' },                // Beer Bottles
+        { id: 1, key: 'spiritBottle' },              // Spirit Bottles
+        { id: 2, key: 'wineBottle' },                // Wine Bottles
+        { id: 3, key: 'beerCan' },                   // Beer Cans
+        { id: 4, key: 'brokenGlass' },               // Broken Glass
+        { id: 5, key: 'bottleTops' },                // Beer bottle tops
+        { id: 6, key: 'paperCardAlcoholPackaging' }, // Paper Packaging
+        { id: 7, key: 'plasticAlcoholPackaging' },   // Plastic Packaging
+        { id: 8, key: 'alcoholOther' },              // Alcohol-Other
+        { id: 9, key: 'pint' },                      // Pint Glass
+        { id: 10, key: 'six_pack_rings' },           // Six-pack rings
+        { id: 11, key: 'alcohol_plastic_cups' }      // Plastic Cups
+    ],
+    coffee: [
+        { id: 0, key: 'coffeeCups' }, // Coffee Cups
+        { id: 1, key: 'coffeeLids' }, // Coffee Lids
+        { id: 2, key: 'coffeeOther' } // Coffee-Other
+    ],
+    food: [
+        { id: 0, key: 'sweetWrappers' },        // Sweet Wrappers
+        { id: 1, key: 'paperFoodPackaging' },   // Paper/Cardboard Packaging
+        { id: 2, key: 'plasticFoodPackaging' }, // Plastic Packaging
+        { id: 3, key: 'plasticCutlery' },       // Plastic Cutlery
+        { id: 4, key: 'crisp_small' },          // Crisp/Chip Packet (small)
+        { id: 5, key: 'crisp_large' },          // Crisp/Chip Packet (large)
+        { id: 6, key: 'styrofoam_plate' },      // Styrofoam Plate
+        { id: 7, key: 'napkins' },              // Napkins
+        { id: 8, key: 'sauce_packet' },         // Sauce Packet
+        { id: 9, key: 'glass_jar' },            // Glass Jar
+        { id: 10, key: 'glass_jar_lid' },       // Glass Jar Lid
+        { id: 11, key: 'foodOther' }            // Food-other
+    ],
+	softdrinks: [
+        { id: 0, key: 'waterBottle' },      // Plastic Water bottle
+        { id: 1, key: 'fizzyDrinkBottle' }, // Plastic Fizzy Drink bottle
+        { id: 2, key: 'tinCan' },           // Can
+        { id: 3, key: 'bottleLid' },        // Bottle Tops
+        { id: 4, key: 'bottleLabel' },      // Bottle Labels
+        { id: 5, key: 'sportsDrink' },      // Sports Drink bottle
+        { id: 6, key: 'straws' },           // Straws
+        { id: 7, key: 'plastic_cups' },     // Plastic Cups
+        { id: 8, key: 'plastic_cup_tops' }, // Plastic Cup Tops
+        { id: 9, key: 'milk_bottle' },      // Milk Bottle
+        { id: 10, key: 'milk_carton' },      // Milk Carton
+        { id: 11, key: 'paper_cups' },       // Paper Cups
+        { id: 12, key: 'juice_cartons' },    // Juice Cartons
+        { id: 13, key: 'juice_bottles' },    // Juice Bottles
+        { id: 14, key: 'juice_packet' },     // Juice Packet
+        { id: 15, key: 'ice_tea_bottles' },  // Ice Tea Bottles
+        { id: 16, key: 'ice_tea_can' },      // Ice Tea Can
+        { id: 17, key: 'energy_can' },       // Energy Can
+        { id: 18, key: 'pullring' },         // Pull-ring
+        { id: 19, key: 'strawpacket' },      // Straw Packaging
+        { id: 20, key: 'styro_cup' },        // Styrofoam Cup
+        { id: 21, key: 'softDrinkOther' }    // Soft Drink (other)
+    ],
+	sanitary: [
+        { id: 0, key: 'gloves' },       // Gloves
+        { id: 1, key: 'facemask' },     // Facemask
+        { id: 2, key: 'condoms' },      // Condoms
+        { id: 3, key: 'nappies' },      // Nappies
+        { id: 4, key: 'menstral' },     // Menstral
+        { id: 5, key: 'deodorant' },    // Deodorant
+        { id: 6, key: 'ear_swabs' },    // Ear Swabs
+        { id: 7, key: 'tooth_pick' },   // Tooth Pick
+        { id: 8, key: 'tooth_brush' },  // Tooth Brush
+        { id: 9, key: 'wetwipes' },     // Wet Wipes
+        { id: 10, key: 'sanitaryOther' } // Sanitary (other)
+    ],
+	other: [
+        { id: 0, key: 'dogshit' },          // Pet Surprise
+        { id: 1, key: 'pooinbag' },         // Surprise In Bag
+        { id: 2, key: 'automobile' },       // Automobile
+        { id: 3, key: 'clothing' },         // Clothing
+        { id: 4, key: 'traffic_cone' },     // Traffic cone
+        { id: 5, key: 'life_buoy' },        // Life Buoy
+        { id: 6, key: 'plastic' },          // Unidentified Plastic
+        { id: 7, key: 'dump' },             // Illegal Dumping
+        { id: 8, key: 'metal' },            // Metal Object
+        { id: 9, key: 'plastic_bags' },     // Plastic Bags
+        { id: 10, key: 'election_posters' }, // Election Posters
+        { id: 11, key: 'forsale_posters' },  // For Sale Posters
+        { id: 12, key: 'books' },            // Books
+        { id: 13, key: 'magazine' },         // Magazines
+        { id: 14, key: 'paper' },            // Paper
+        { id: 15, key: 'stationary' },       // Stationery
+        { id: 16, key: 'washing_up' },       // Washing-up Bottle
+        { id: 17, key: 'hair_tie' },         // Hair Tie
+        { id: 18, key: 'ear_plugs' },        // Ear Plugs (music)
+        { id: 19, key: 'batteries' },        // Batteries
+        { id: 20, key: 'elec_small' },       // Electric small
+        { id: 21, key: 'elec_large' },       // Electric large
+        { id: 22, key: 'other' },            // Other (other)
+        { id: 23, key: 'random_litter' },    // Random Litter
+    ],
+	dumping: [
+        { id: 0, key: 'small' },  // Small
+        { id: 2, key: 'medium' }, // Medium
+        { id: 3, key: 'large' },  // Large
+    ],
+	industrial: [
+        { id: 0, key: 'oil' },                // Oil
+        { id: 1, key: 'industrial_plastic' }, // Plastic
+        { id: 2, key: 'chemical' },           // Chemical
+        { id: 3, key: 'bricks' },             // Bricks
+        { id: 4, key: 'tape' },               // Tape
+        { id: 5, key: 'industrial_other' }    // Other
+    ],
+	coastal: [
+        { id: 0, key: 'microplastics' },            // Microplastics
+        { id: 1, key: 'mediumplastics' },           // Mediumplastics
+        { id: 2, key: 'macroplastics' },            // Macroplastics
+        { id: 3, key: 'rope_small' },               // Rope small
+        { id: 4, key: 'rope_medium' },              // Rope medium
+        { id: 5, key: 'rope_large' },               // Rope large
+        { id: 6, key: 'fishing_gear_nets' },        // Fishing gear/nets
+        { id: 7, key: 'buoys' },                    // Buoys
+        { id: 8, key: 'degraded_plasticbottle' },   // Degraded Plastic Bottle
+        { id: 9, key: 'degraded_plasticbag' },      // Degraded Plastic Bag
+        { id: 10, key: 'degraded_straws' },         // Degraded Drinking Straws
+        { id: 11, key: 'degraded_lighters' },       // Degraded Lighters
+        { id: 12, key: 'balloons' },                // Balloons
+        { id: 13, key: 'lego' },                    // Lego
+        { id: 14, key: 'shotgun_cartridges' },      // Shotgun Cartridges
+        { id: 15, key: 'styro_small' },             // Styrofoam small
+        { id: 16, key: 'styro_medium' },            // Styrofoam medium
+        { id: 17, key: 'styro_large' },             // Styrofoam large
+        { id: 18, key: 'coastal_other' }            // Coastal (other
+    ],
+	brands: [
+        { id: 0, key: 'adidas' },           // Adidas
+        { id: 1, key: 'amazon' },           // Amazon
+        { id: 2, key: 'apple' },            // Apple
+        { id: 3, key: 'budweiser' },        // Budweiser
+        { id: 4, key: 'camel' },            // Camel
+        { id: 5, key: 'coke' },             // Coca-Cola
+        { id: 6, key: 'colgate' },          // Colgate
+        { id: 7, key: 'corona' },           // Corona
+        { id: 8, key: 'costa' },            // Costa
+        { id: 9, key: 'doritos' },          // Doritos
+        { id: 10, key: 'drpepper' },         // DrPepper
+        { id: 11, key: 'dunnes' },           // Dunnes
+        { id: 12, key: 'duracell' },         // Duracell
+        { id: 13, key: 'durex' },            // Durex
+        { id: 14, key: 'esquires' },         // Esquires
+        { id: 15, key: 'frank_and_honest' }, // Frank-and-Honest
+        { id: 16, key: 'fritolay' },         // Frito-Lay
+        { id: 17, key: 'gatorade' },         // Gatorade
+        { id: 18, key: 'gillette' },         // Gillette
+        { id: 19, key: 'haribo' },           // Haribo
+        { id: 20, key: 'heineken' },         // Heineken
+        { id: 21, key: 'insomnia' },         // Insomnia
+        { id: 22, key: 'kellogs:' },         // Kellogs
+        { id: 23, key: 'kfc' },              // KFC
+        { id: 24, key: 'lego' },             //  'Lego', -> duplicated by Coast
+        { id: 25, key: 'lidl' },             // Lidl
+        { id: 26, key: 'lindenvillage' },    // Lindenvillage
+        { id: 27, key: 'lolly_and_cookes' }, // Lolly-and-cookes
+        { id: 28, key: 'loreal' },           // Loreal
+        { id: 29, key: 'lucozade' },         // Lucozade
+        { id: 30, key: 'nescafe' },          // Nescafe
+        { id: 31, key: 'nestle' },           // Nestle
+        { id: 32, key: 'marlboro' },         // Marlboro
+        { id: 33, key: 'mcdonalds' },        // McDonalds
+        { id: 34, key: 'nike' },             // Nike
+        { id: 35, key: 'obriens' },          // O-Briens
+        { id: 36, key: 'pepsi' },            // Pepsi
+        { id: 37, key: 'powerade' },         // Powerade
+        { id: 38, key: 'redbull' },          // Redbull
+        { id: 39, key: 'ribena' },           // Ribena
+        { id: 40, key: 'samsung' },          // Samsung
+        { id: 41, key: 'sainsburys' },       // Sainsburys
+        { id: 42, key: 'spar' },             // Spar
+        { id: 43, key: 'stella' },           // Stella
+        { id: 44, key: 'subway' },           // Subway
+        { id: 45, key: 'supermacs' },        // Supermacs
+        { id: 46, key: 'supervalu' },        // Supervalu
+        { id: 47, key: 'starbucks' },        // Starbucks
+        { id: 48, key: 'tayto' },            // Tayto
+        { id: 49, key: 'tesco' },            // Tesco
+        { id: 50, key: 'thins' },            // Thins
+        { id: 51, key: 'volvic' },           // Volvic
+        { id: 52, key: 'waitrose' },         // Waitrose
+        { id: 53, key: 'walkers' },          // Walkers
+        { id: 54, key: 'woolworths' },       // Woolworths
+        { id: 55, key: 'wilde_and_greene' }, // Wilde-and-Greene
+        { id: 56, key: 'wrigleys' },         // Wrigley
+    ],
+    trashdog: [
+        { id: 0, key: 'trashdog' }, // TrashDog
+        { id: 1, key: 'littercat' }, // LitterCat
+        { id: 2, key: 'duck:' } // LitterDuc
+    ]
 };
