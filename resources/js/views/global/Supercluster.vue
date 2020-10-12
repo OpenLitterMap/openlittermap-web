c3430f3bd4dd5cb810c1e443a7afd0932e17b5ad
xlcrr <info@openlittermap.com>
2020-10-12 22:12:00 +0100

    add supercluster

--- resources/js/views/global/Supercluster.vue
+++ resources/js/views/global/Supercluster.vue
@@ -29,24 +29,41 @@ var map;
 var index;
 var markers;
 
+const single_icon = L.icon({
+    iconUrl: './images/vendor/leaflet/dist/dot.png',
+    iconSize: [10, 10]
+});
+
 function createClusterIcon (feature, latlng)
 {
-    // console.log({ feature })
-    if (! feature.properties.cluster) return L.marker(latlng);
+    if (! feature.properties.cluster) return L.marker(latlng, { icon: single_icon });
 
     let count = feature.properties.point_count;
     let size = count < 100 ? 'small' : count < 1000 ? 'medium' : 'large';
 
     let icon = L.divIcon({
         html: '<div class="mi"><span class="ma">' + feature.properties.point_count_abbreviated + '</span></div>',
-        // className: 'marker-cluster marker-et-' + size,
         className: 'marker-cluster-' + size,
         iconSize: L.point(40, 40)
     });
 
-    return L.marker(latlng, {
-        icon: icon
-    });
+    return L.marker(latlng, { icon });
+}
+
+/**
+ * The user dragged or zoomed the map
+ */
+function update ()
+{
+    const bounds = map.getBounds();
+
+    let bbox = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()];
+    let zoom = map.getZoom();
+
+    let clusters = index.getClusters(bbox, zoom)
+
+    markers.clearLayers();
+    markers.addData(clusters);
 }
 
 export default {
@@ -60,7 +77,7 @@ export default {
     mounted ()
     {
         /** 1. Create map object */
-        map = L.map(this.$refs.map, {
+        map = L.map('super', {
             center: [0, 0],
             zoom: 2,
         });
@@ -78,24 +95,63 @@ export default {
 
         map.attributionControl.addAttribution('Litter data &copy OpenLitterMap & Contributors ' + year);
 
-        // Create clusters object
-        index = new Supercluster({
-            radius: 40,
-            maxZoom: 16
-        });
-
-        index.load(this.geojson.features);
-        index.getClusters([-180, -85, 180, 85], 2);
-        console.log({ index });
-
         // Empty Layer Group that will receive the clusters data on the fly.
         markers = L.geoJSON(null, {
             pointToLayer: createClusterIcon
         }).addTo(map);
+
+        // Create clusters object
+        index = new Supercluster({
+            log: true,
+            radius: 40,
+            maxZoom: 16
+        });
+
+        index.load(this.$store.state.globalmap.geojson.features);
+
+        let clusters = index.getClusters([-180, -85, 180, 85], 2);
+
+        markers.addData(clusters);
+
+        map.on('moveend', function () {
+            update();
+        });
+
+        // todo - getClusterExpansionZoom(clusterId);
+    },
+
+    methods: {
+
+        /**
+         * Close dates and language dropdowns
+         */
+        closeButtons ()
+        {
+            this.$store.commit('closeDatesButton');
+            this.$store.commit('closeLangsButton');
+        }
     }
 }
 </script>
 
-<style scoped>
+<style>
+
+    #super {
+        height: 100%;
+        margin: 0;
+        position: relative;
+    }
+
+    .leaflet-marker-icon {
+        border-radius: 20px;
+    }
+
+    .mi {
+        height: 100%;
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        border-radius: 20px;
+    }
 
 </style>
