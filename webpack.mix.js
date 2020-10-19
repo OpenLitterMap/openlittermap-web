ef9d9bb2fa43a855f0e37a5920d3a71eb5bfa638
xlcrr <info@openlittermap.com>
2020-10-19 20:38:01 +0100

    remove comments nrp

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,19 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// remove comments when in production
+if (mix.inProduction())
+{
+    mix.options({
+        terser: {
+            terserOptions: {
+                compress: {
+                    drop_console: true
+                }
+            }
+        }
+    });
+
+    mix.version();
+}
