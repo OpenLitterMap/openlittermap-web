4bb44c6f9178ad44103111daa579f92663609d57
xlcrr <info@openlittermap.com>
2020-10-23 10:36:07 +0100

    get ids

--- app/Console/Commands/SendEmailToAll.php
+++ app/Console/Commands/SendEmailToAll.php
@@ -40,7 +40,7 @@ class SendEmailToAll extends Command
     public function handle()
     {
         $users = User::where([
-            ['id', '>', 547],
+            ['id', '>', 871],
             'emailsub' => 1
         ])->orderBy('id', 'asc')->get();
 
