3874872491a40354c223b744fff0e656d1e2239c
xlcrr <info@openlittermap.com>
2020-11-16 20:04:38 +0000

    Add language button

---
+++ resources/js/langs/es/home/index.js
@@ -0,0 +1,11 @@
+import welcome from './welcome.json'
+import footer from './footer.json'
+import donate from './donate.json'
+import about from './about.json'
+
+export const home = {
+    about,
+    donate,
+    footer,
+    welcome
+}
