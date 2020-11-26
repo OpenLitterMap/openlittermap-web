3ea430acc702e63afa5fde53cdacc31ad3f5b9c8
Trung Luu <lhtrung307@gmail.com>
2020-11-27 00:21:34 +0700

    Add vue select to help user search for tags and improve delete button

--- resources/js/views/general/Upload.vue
+++ resources/js/views/general/Upload.vue
@@ -1,33 +1,34 @@
 <template>
     <section class="section hero fullheight is-warning is-bold" style="padding: 5em;">
-
         <div class="container ma has-text-centered" style="flex-grow: 0; width: 100%;">
-
-            <h1 class="title is-1 drop-title">Click to upload or drop your photos</h1>
+            <h1 class="title is-1 drop-title">
+                Click to upload or drop your photos
+            </h1>
 
             <vue-dropzone id="dropzone" class="mb3" :options="options" />
 
-            <h2 class="title is-2">Thank you!</h2>
+            <h2 class="title is-2">
+                Thank you!
+            </h2>
 
-            <h3 class="title is-3 mb2r">Next, you need to tag the litter</h3>
+            <h3 class="title is-3 mb2r">
+                Next, you need to tag the litter
+            </h3>
 
-            <button class="button is-medium is-info hov" @click="tag">Tag Litter</button>
+            <button class="button is-medium is-info hov" @click="tag">
+                Tag Litter
+            </button>
         </div>
     </section>
 </template>
 
 <script>
-import vue2Dropzone from 'vue2-dropzone'
+import vue2Dropzone from 'vue2-dropzone';
 
 export default {
     name: 'Upload',
     components: {
         vueDropzone: vue2Dropzone
-    },
-    async created ()
-    {
-        // user object is not passed when the user logs in. We need to get it here
-        if (Object.keys(this.$store.state.user.user.length === 0)) await this.$store.dispatch('GET_CURRENT_USER');
     },
     data ()
     {
@@ -43,7 +44,12 @@ export default {
                 duplicateCheck: true,
                 paramName: 'file'
             }
-        }
+        };
+    },
+    async created ()
+    {
+        // user object is not passed when the user logs in. We need to get it here
+        if (Object.keys(this.$store.state.user.user.length === 0)) await this.$store.dispatch('GET_CURRENT_USER');
     },
     methods: {
 
@@ -55,7 +61,7 @@ export default {
             this.$router.push({ path: '/tag' });
         }
     }
-}
+};
 </script>
 
 <style scoped>
