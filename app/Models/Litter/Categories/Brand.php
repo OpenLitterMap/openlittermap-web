5b489c60402890d4d92063ff43af1d2f290293b0
xlcrr <info@openlittermap.com>
2020-11-29 20:26:37 +0000

    add missing brands types to calculate total

--- app/Models/Litter/Categories/Brand.php
+++ app/Models/Litter/Categories/Brand.php
@@ -2,9 +2,9 @@
 
 namespace App\Models\Litter\Categories;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Litter\LitterCategory;
 
-class Brand extends Model
+class Brand extends LitterCategory
 {
     protected $fillable = [
         'photo_id',
@@ -105,4 +105,108 @@ class Brand extends Model
         'wilde_and_greene',
         'wrigleys'
     ];
+
+    /**
+     * Pre-defined litter types available on this class
+     */
+    public function types ()
+    {
+        return [
+            'adidas',
+            'amazon',
+            'aldi',
+            'apple',
+            'applegreen',
+            'asahi',
+            'avoca',
+
+            'ballygowan',
+            'bewleys',
+            'brambles',
+            'budweiser',
+            'bulmers',
+            'burgerking',
+            'butlers',
+
+            'cadburys',
+            'cafe_nero',
+            'camel',
+            'carlsberg',
+            'centra',
+            'coke',
+            'circlek',
+            'coles',
+            'colgate',
+            'corona',
+            'costa',
+
+            'doritos',
+            'drpepper',
+            'dunnes',
+            'duracell',
+            'durex',
+
+            'esquires',
+
+            'frank_and_honest',
+            'fritolay',
+
+            'gatorade',
+            'gillette',
+            'guinness',
+
+            'haribo',
+            'heineken',
+
+            'insomnia',
+
+            'kellogs',
+            'kfc',
+
+            'lego',
+            'lidl',
+            'lindenvillage',
+            'lolly_and_cookes',
+            'loreal',
+            'lucozade',
+
+            'marlboro',
+            'mars',
+            'mcdonalds',
+
+            'nero',
+            'nescafe',
+            'nestle',
+            'nike',
+
+            'obriens',
+
+            'pepsi',
+            'powerade',
+
+            'redbull',
+            'ribena',
+
+            'samsung',
+            'sainsburys',
+            'spar',
+            'stella',
+            'subway',
+            'supermacs',
+            'supervalu',
+            'starbucks',
+
+            'tayto',
+            'tesco',
+            'thins',
+
+            'volvic',
+
+            'waitrose',
+            'walkers',
+            'woolworths',
+            'wilde_and_greene',
+            'wrigleys'
+        ];
+    }
 }
