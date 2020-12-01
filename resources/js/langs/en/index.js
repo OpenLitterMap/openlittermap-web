3e2e421a3755732c9a5aff33a4cfe7a2022da738
Trung Luu <lhtrung307@gmail.com>
2020-12-02 02:29:36 +0700

    Fix eslint

--- resources/js/langs/en/index.js
+++ resources/js/langs/en/index.js
@@ -1,17 +1,17 @@
-import common from "./common.json";
-import creditcard from "./creditcard.json";
-import litter from "./litter.json";
-import location from "./location.json";
-import notifications from "./notifications.json";
-import profile from "./profile.json";
-import { settings } from "./settings/index";
-import { locations } from "./locations/index";
-import { home } from "./home/index";
-import signup from "./signup.json";
-import tags from "./tags.json";
-import teams from "./teams.json";
-import nav from "./nav.json";
-import upload from "./upload.json";
+import common from './common.json';
+import creditcard from './creditcard.json';
+import litter from './litter.json';
+import location from './location.json';
+import notifications from './notifications.json';
+import profile from './profile.json';
+import { settings } from './settings/index';
+import { locations } from './locations/index';
+import { home } from './home/index';
+import signup from './signup.json';
+import tags from './tags.json';
+import teams from './teams.json';
+import nav from './nav.json';
+import upload from './upload.json';
 
 export const en = {
     common,
