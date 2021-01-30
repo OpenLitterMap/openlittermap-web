058c1a779011d9ebc7eacb996ee8596b15fee611
xlcrr <info@openlittermap.com>
2021-01-30 13:19:00 +0000

    type to select tags

--- resources/js/store/modules/litter/init.js
+++ resources/js/store/modules/litter/init.js
@@ -1,8 +1,8 @@
 export const init = {
-    category: {}, // currently selected category.
+    category: 'smoking', // currently selected category.
     hasAddedNewTag: false, // Has the admin added a new tag yet? If FALSE, disable "Update With New Tags button"
     presence: null, // true = remaining
-    item: {}, // currently selected item
+    item: 'butts', // currently selected item
     items: [], // items for the currently selected category
     loading: false,
     photos: {}, // paginated photos object
