026877ee894f46d14323c9e599881966f4c126e9
William Alexander <willalexander86@gmail.com>
2021-02-03 23:39:33 +0000

    Implemented data download option for non-users

--- resources/js/components/Locations/Charts/Download/Download.vue
+++ resources/js/components/Locations/Charts/Download/Download.vue
@@ -3,7 +3,21 @@
         <h1 class="title is-3">Free and Open Verified Citizen Science Data on Plastic Pollution.</h1>
         <h1 class="title is-3">Let's stop plastic going into the ocean.</h1>
 
-        <button class="button is-large is-danger mb1" @click="download">Download</button>
+        <p class="mb1" v-show="!isAuth">Please enter an email address to which the data will be sent:</p>
+
+        <input          
+            v-show="!isAuth"
+            class="input mb1em fs125"
+            placeholder="you@email.com"
+            type="email"
+            name="email"
+            required
+            v-model="email"
+            @keydown="textEntered"
+            autocomplete="email"
+        />
+
+        <button :disabled="disableDownloadButton" class="button is-large is-danger mb1" @click="download">Download</button>
 
         <p>&copy; OpenLitterMap & Contributors.</p>
     </div>
@@ -16,7 +30,8 @@ export default {
     data ()
     {
         return {
-            email: ''
+            email: '',
+            emailEntered: false
         };
     },
     methods: {
@@ -32,10 +47,37 @@ export default {
         {
             await this.$store.dispatch('DOWNLOAD_DATA', {
                 type: this.type,
-                locationId: this.locationId
+                locationId: this.locationId,
+                email: this.email
             });
         },
+
+        textEntered()
+        {
+            this.emailEntered = true;
+        }
     },
+
+    computed: {
+
+        isAuth ()
+        {
+            return this.$store.state.user.auth;
+        },
+
+        disableDownloadButton()
+        {
+            if(this.isAuth)
+            {
+                return false;
+            }
+
+            else 
+            {
+                return !this.emailEntered;
+            }
+        }
+    }
 }
 </script>
 
