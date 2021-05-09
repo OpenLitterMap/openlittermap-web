a600874431ffaacfb5b84164520174ddbcd253ea
xlcrr <info@openlittermap.com>
2021-05-09 20:40:45 +0100

    fix php version issue

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Route;
 //    return view('emails.update23', ['user' => $user]);
 //});
 
+Route::get('/test', function () {
+    return 'hello';
+});
+
 Route::get('/', 'HomeController@index');
 Route::get('/about', 'HomeController@index');
 Route::get('/world', 'HomeController@index');
