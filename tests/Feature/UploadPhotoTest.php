67f9e0f58489c58190a2e72910cfa0c7288351c9
geni_jaho <jahogeni@gmail.com>
2021-06-17 14:51:51 +0200

    Added tests for image uploading to S3 on production

--- tests/Feature/UploadPhotoTest.php
+++ tests/Feature/UploadPhotoTest.php
@@ -6,6 +6,7 @@ use App\Events\ImageUploaded;
 use App\Events\Photo\IncrementPhotoMonth;
 use App\Models\Teams\Team;
 use App\Models\User\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Event;
@@ -18,6 +19,8 @@ use App\Models\Photo;
 
 class UploadPhotoTest extends TestCase
 {
+    use WithoutMiddleware;
+
     private $imagePath;
 
     protected function setUp(): void
@@ -238,11 +241,81 @@ class UploadPhotoTest extends TestCase
 
     public function test_it_uploads_the_photo_to_s3_on_production()
     {
-        $this->markTestIncomplete();
+        Carbon::setTestNow();
+
+        Storage::fake('s3');
+
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $file = UploadedFile::fake()->createWithContent(
+            'image.jpg',
+            file_get_contents($this->imagePath)
+        );
+
+        $dateTime = now();
+        $year = $dateTime->year;
+        $month = $dateTime->month < 10 ? "0$dateTime->month" : $dateTime->month;
+        $day = $dateTime->day < 10 ? "0$dateTime->day" : $dateTime->day;
+
+        $filepath = "$year/$month/$day/{$file->hashName()}";
+        $imageName = Storage::disk('s3')->url($filepath);
+
+        app()->detectEnvironment(function () {
+            return 'production';
+        });
+
+        $response = $this->post('/submit', [
+            'file' => $file,
+        ]);
+
+        $response->assertOk()->assertJson(['msg' => 'success']);
+
+        // Image is uploaded
+        Storage::disk('s3')->assertExists($filepath);
+
+        // User info gets updated
+        $user->refresh();
+        $this->assertEquals(1, $user->has_uploaded);
+        $this->assertEquals(1, $user->xp);
+        $this->assertEquals(1, $user->total_images);
+
+        // The Photo is persisted correctly
+        $this->assertCount(1, $user->photos);
+
+        $photo = $user->photos->first();
+        $this->assertEquals($imageName, $photo->filename);
+        $this->assertEquals($imageName, $photo->five_hundred_square_filepath);
     }
 
-    public function test_it_throws_server_error_when_user_uploads_the_same_photo_on_production()
+    public function test_it_throws_server_error_when_user_uploads_photos_with_the_same_datetime_on_production()
     {
-        $this->markTestIncomplete();
+        Carbon::setTestNow();
+
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        Photo::factory()->create([
+            'user_id' => $user->id,
+            'datetime' => now()
+        ]);
+
+        $file = UploadedFile::fake()->createWithContent(
+            'image.jpg',
+            file_get_contents($this->imagePath)
+        );
+
+        app()->detectEnvironment(function () {
+            return 'production';
+        });
+
+        $response = $this->post('/submit', [
+            'file' => $file,
+        ]);
+
+        $response->assertStatus(500);
+        $response->assertSee('Server Error');
     }
 }
