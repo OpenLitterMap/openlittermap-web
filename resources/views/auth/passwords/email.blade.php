125cf13d49564f2f9ee80c613a62d0797878335d
geni_jaho <jahogeni@gmail.com>
2021-09-01 13:45:21 +0200

    Fixed Reset Passwords email page layout

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,41 +1,53 @@
 @extends('app')
 
 @section('content')
-<section class="hero is-info is-fullheight">
-    <div class="columns">
-        <div class="column is-half is-offset-3 has-text-centered" style="margin-top: 10%;">
-            <p class="title is-1">Reset Password</p>
-            <div class="panel-body">
-                @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-                @endif
-
-                <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                    {{ csrf_field() }}
-
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email">E-Mail Address</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="input" name="email" value="{{ old('email') }}" required placeholder="you@email.com" style="width: 50%;" />
-
-                            @if ($errors->has('email'))
-                            <br>
-                            <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
+    <section class="hero is-info is-fullheight">
+        <div class="columns" style="width: 100%;margin-top: auto;margin-bottom: auto;margin-left: 0;margin-right: 0;">
+            <div class="column"></div>
+            <div class="column is-one-quarter">
+                <p class="title is-1 has-text-centered">Reset Password</p>
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
                         </div>
-                    </div>
+                    @endif
 
-                    <button type="submit" class="button is-primary" style="margin-top: 2em;">
-                        Send Password Reset Link
-                    </button>
-                </form>
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+                        {{ csrf_field() }}
+
+                        <div class="field" style="margin-right:24px;margin-left: 24px;">
+                            <label class="label has-text-white" for="email">E-Mail Address</label>
+
+                            <div class="control">
+                                <input
+                                    id="email"
+                                    type="email"
+                                    class="input {{ $errors->has('email') ? 'is-danger' : '' }}"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    required
+                                    autofocus
+                                    placeholder="you@email.com"
+                                />
+
+                                @if ($errors->has('email'))
+                                    <p class="help has-text-white has-text-weight-bold">{{ $errors->first('email') }}</p>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="field has-text-centered">
+                            <div class="control">
+                                <button type="submit" class="button is-primary">
+                                    Send Password Reset Link
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
+            <div class="column"></div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
