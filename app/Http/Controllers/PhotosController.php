0f5d40afc6550b3807dac4474f65a2d6618491fc
geni_jaho <jahogeni@gmail.com>
2021-09-24 20:18:43 +0200

    Fixed required mime types

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -88,7 +88,7 @@ class PhotosController extends Controller
     public function store (Request $request)
     {
         $request->validate([
-           'file' => 'required'
+           'file' => 'required|mimes:jpg,png,jpeg,heif,heic'
         ]);
 
         $user = Auth::user();
