a2738f1e3f4121add957b7d320494f09f995cb7d
xlcrr <info@openlittermap.com>
2021-12-02 10:07:28 +0000

    increase android store version

--- app/Http/Controllers/API/MobileAppVersionController.php
+++ app/Http/Controllers/API/MobileAppVersionController.php
@@ -18,7 +18,7 @@ class MobileAppVersionController extends Controller
             ],
             'android' => [
                 'url' => 'https://play.google.com/store/apps/details?id=com.geotech.openlittermap',
-                'version' => '3.0.0'
+                'version' => '3.0.2'
             ]
         ];
     }
