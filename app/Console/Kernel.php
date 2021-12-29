c444ababfaa5ada146d19aa5acc1fb6b45457fe0
xlcrr <info@openlittermap.com>
2021-12-29 15:10:48 +0000

    regenerate clusters daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sitemap:generate')->daily();
-        $schedule->command('clusters:generate-all')->mondays();
+        $schedule->command('clusters:generate-all')->daily();
     }
 
     /**
