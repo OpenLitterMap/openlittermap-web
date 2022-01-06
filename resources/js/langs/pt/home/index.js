319a281321b4d6a42320bb31655a2af051fe5b58
Sthefane <sthefanesoliveiraa@outlook.com>
2022-01-06 13:06:10 -0300

    adding Portuguese

---
+++ resources/js/langs/pt/home/index.js
@@ -0,0 +1,11 @@
+import about from './about.json'
+import donate from './donate.json'
+import footer from './footer.json'
+import welcome from './welcome.json'
+
+export const home = {
+    about,
+    donate,
+    footer,
+    welcome
+}
