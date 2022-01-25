42257b33c224b6854d088ef5d3e8884386292fed
geni_jaho <jahogeni@gmail.com>
2022-01-25 18:13:11 +0100

    Added command to update redis locations xp

---
+++ app/Console/Commands/Users/UpdateRedisLocationsXp.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Console\Commands\Users;
+
+use App\Actions\Locations\UpdateLeaderboardsForLocationAction;
+use App\Models\Photo;
+use App\Models\User\User;
+use Illuminate\Console\Command;
+
+class UpdateRedisLocationsXp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:update-redis-locations-xp';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Recalculates users xp based on their photos and tags';
+
+    /**
+     * @var UpdateLeaderboardsForLocationAction
+     */
+    private $updateLeaderboardsAction;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(UpdateLeaderboardsForLocationAction $action)
+    {
+        parent::__construct();
+        $this->updateLeaderboardsAction = $action;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->withProgressBar(User::all(), function ($user) {
+            $user->photos()
+                ->with(Photo::categories())
+                ->lazyById()
+                ->each(function (Photo $photo) {
+                    $xp = $this->calculateXp($photo);
+
+                    $this->updateLeaderboardsAction->run($photo, $photo->user_id, $xp);
+                });
+        });
+
+        return 0;
+    }
+
+    /**
+     * @param Photo $photo
+     * @return int
+     */
+    private function calculateXp(Photo $photo): int
+    {
+        $xpFromTags = (int) collect($photo->categories())
+            ->filter(function ($category) use ($photo) {
+                return $photo->$category;
+            })
+            ->sum(function ($category) use ($photo) {
+                return $photo->$category->total();
+            });
+
+        return 1 + $xpFromTags;
+    }
+}
---
+++ tests/Unit/Commands/RecalculateUsersXpTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Unit\Commands;
+
+use App\Models\Litter\Categories\Smoking;
+use App\Models\Location\City;
+use App\Models\Location\Country;
+use App\Models\Location\State;
+use App\Models\Photo;
+use App\Models\User\User;
+use Illuminate\Support\Facades\Redis;
+use Tests\TestCase;
+
+class RecalculateUsersXpTest extends TestCase
+{
+    public function test_it_recalculates_users_xp_by_location()
+    {
+        list($country1, $state1, $city1) = $this->createLocation();
+        list($country2, $state2, $city2) = $this->createLocation();
+        $user = User::factory()->create();
+        $photo1 = Photo::factory()->create([
+            'user_id' => $user->id,
+            'smoking_id' => Smoking::factory()->create(['butts' => 3])->id,
+            'country_id' => $country1->id,
+            'state_id' => $state1->id,
+            'city_id' => $city1->id
+        ]);
+        $photo2 = Photo::factory()->create([
+            'user_id' => $user->id,
+            'smoking_id' => Smoking::factory()->create(['butts' => 5])->id,
+            'country_id' => $country2->id,
+            'state_id' => $state2->id,
+            'city_id' => $city2->id
+        ]);
+        $this->clearRedisLocation($country1, $state1, $city1);
+        $this->clearRedisLocation($country2, $state2, $city2);
+        $this->assertRedisLocationEquals(0, $user, $country1, $state1, $city1);
+        $this->assertRedisLocationEquals(0, $user, $country2, $state2, $city2);
+
+        $this->artisan('users:update-redis-locations-xp');
+
+        $this->assertRedisLocationEquals(4, $user, $country1, $state1, $city1);
+        $this->assertRedisLocationEquals(6, $user, $country2, $state2, $city2);
+    }
+
+    /**
+     * @return array
+     */
+    private function createLocation(): array
+    {
+        $country = Country::factory()->create(['shortcode' => 'us', 'country' => 'USA']);
+        $state = State::factory()->create(['state' => 'North Carolina', 'country_id' => $country->id]);
+        $city = City::factory()->create(['city' => 'Swain County', 'country_id' => $country->id, 'state_id' => $state->id]);
+        return array($country, $state, $city);
+    }
+
+    /**
+     * @param $country
+     * @param $state
+     * @param $city
+     * @return void
+     */
+    private function clearRedisLocation($country, $state, $city): void
+    {
+        Redis::del("xp.country.$country->id");
+        Redis::del("xp.country.$country->id.state.$state->id");
+        Redis::del("xp.country.$country->id.state.$state->id.city.$city->id");
+    }
+
+    /**
+     * @param $expected
+     * @param $user
+     * @param $country
+     * @param $state
+     * @param $city
+     * @return void
+     */
+    private function assertRedisLocationEquals($expected, $user, $country, $state, $city): void
+    {
+        $this->assertEquals($expected, Redis::zscore("xp.country.$country->id", $user->id));
+        $this->assertEquals($expected, Redis::zscore("xp.country.$country->id.state.$state->id", $user->id));
+        $this->assertEquals($expected, Redis::zscore("xp.country.$country->id.state.$state->id.city.$city->id", $user->id));
+    }
+}
