f95e60e23f33cdf9a436777c108d34f710f138e8
xlcrr <info@openlittermap.com>
2022-01-29 19:41:15 +0000

    compile locally

--- resources/js/components/Locations/Charts/Leaderboard/Leaderboard.vue
+++ resources/js/components/Locations/Charts/Leaderboard/Leaderboard.vue
@@ -22,8 +22,7 @@ w<template>
 export default {
 	name: 'Leaderboard',
 	props: ['leaderboard'],
-	data ()
-	{
+	data () {
 		return {
             positions: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
         };
