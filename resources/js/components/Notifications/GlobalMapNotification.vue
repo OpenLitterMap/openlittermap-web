85f752411b413e0e638b0cbd81ac5a4c06cdb51e
xlcrr <info@openlittermap.com>
2022-01-31 18:12:10 +0000

    change background of team colour

--- resources/js/components/Notifications/GlobalMapNotification.vue
+++ resources/js/components/Notifications/GlobalMapNotification.vue
@@ -47,8 +47,23 @@ export default {
         background-color: #f1c40f;
     }
 
-    .event-pink {
-        background-color: #e256fff0;
+    .event-colourful {
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 400% 400%;
+        animation: gradient 15s ease infinite;
+    }
+
+    // Make the colourful event animated
+    @keyframes gradient {
+        0% {
+            background-position: 0 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0 50%;
+        }
     }
 
     .event-image {
