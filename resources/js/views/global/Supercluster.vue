61782e0d0079f2d384079ff406ff9a52e51ee082
xlcrr <info@openlittermap.com>
2022-04-18 18:19:49 +0100

    compile for production

--- resources/js/views/global/Supercluster.vue
+++ resources/js/views/global/Supercluster.vue
@@ -109,6 +109,8 @@ function createGlobalGroups ()
         globalLayerController.addOverlay(litterArtPoints, 'Litter Art');
 
         globalControllerShowing = true;
+
+        console.log({ globalLayerController });
     }
 }
 
