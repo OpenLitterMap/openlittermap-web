29632e5a70e2e161101fb576d77c97b0840159eb
xlcrr <info@openlittermap.com>
2022-05-18 08:34:36 +0100

    increase iOS store version

--- app/Http/Controllers/API/MobileAppVersionController.php
+++ app/Http/Controllers/API/MobileAppVersionController.php
@@ -14,7 +14,7 @@ class MobileAppVersionController extends Controller
         return [
             'ios' => [
                 'url' => 'https://apps.apple.com/us/app/openlittermap/id1475982147',
-                'version' => '3.2.2'
+                'version' => '3.2.3'
             ],
             'android' => [
                 'url' => 'https://play.google.com/store/apps/details?id=com.geotech.openlittermap',
