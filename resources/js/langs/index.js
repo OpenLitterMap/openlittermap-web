df3639791cfcf0b8ebb79656fc7b3e5427b5d8a2
xlcrr <info@openlittermap.com>
2022-06-26 01:08:46 +0100

    add change language

--- resources/js/langs/index.js
+++ resources/js/langs/index.js
@@ -1,5 +1,6 @@
 import { en } from './en';
 import { es } from './es';
+import { hu } from './hu';
 import { nl } from './nl';
 import { pl } from './pl';
 import { pt } from './pt';
@@ -7,6 +8,7 @@ import { pt } from './pt';
 export const langs = {
     'en': en,
     'es': es,
+    'hu': hu,
     'nl': nl,
     'pl': pl,
     'pt': pt
