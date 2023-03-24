5a3ff2bbca0f2a41226df4d611a11a0f2174e276
xlcrr <info@openlittermap.com>
2023-03-24 16:44:39 +0000

    add command to reset all Littercoin

---
+++ app/Console/Commands/tmp/ResetLittercoin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands\tmp;
+
+use App\Models\Littercoin;
+use App\Models\Photo;
+use App\Models\User\User;
+use Illuminate\Console\Command;
+
+class ResetLittercoin extends Command
+{
+    protected $signature = 'littercoin:reset';
+
+    protected $description = 'Reset the amount of littercoin owed to all users.';
+
+    public function handle()
+    {
+        Littercoin::truncate();
+
+        $users = User::where('has_uploaded', true)->get();
+
+        foreach ($users as $user)
+        {
+            $photos = Photo::where('user_id', $user->id)
+                ->where('verified', '>=', 2)
+                ->query();
+
+            foreach ($photos as $index => $photo)
+            {
+                if ($index & 100 === 0)
+                {
+                    Littercoin::create([
+                        'user_id' => $user->id,
+                        'photo_id' => $photo->id,
+                        'created_at' => $photo->created_at
+                    ]);
+                }
+            }
+        }
+    }
+}
