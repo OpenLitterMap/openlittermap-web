9391202d272c7e3ba6bed032406c4728def59aa7
Kevin Makwana <kevinmakwana189@gmail.com>
2023-04-03 11:14:19 -0700

    telescope implementation

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
     ],
 
