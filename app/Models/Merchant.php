02b082a642b1f30ce86f33a31ce072513f6a3bad
Sthefane <sthefanesoliveiraa@outlook.com>
2023-04-29 21:33:34 +0100

    stasho

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Merchants extends Model
+class Merchant extends Model
 {
     protected $guarded;
 
