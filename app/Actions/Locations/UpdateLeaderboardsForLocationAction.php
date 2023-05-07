b570caf2f7a6b0e8c4a8ccfb278145ca43830e11
xlcrr <info@openlittermap.com>
2023-05-07 14:59:50 +0100

    download staging

--- app/Actions/Locations/UpdateLeaderboardsForLocationAction.php
+++ app/Actions/Locations/UpdateLeaderboardsForLocationAction.php
@@ -31,5 +31,13 @@ class UpdateLeaderboardsForLocationAction
         Redis::zincrby("xp.country.$photo->country_id", $incrXp, $userId);
         Redis::zincrby("xp.country.$photo->country_id.state.$photo->state_id", $incrXp, $userId);
         Redis::zincrby("xp.country.$photo->country_id.state.$photo->state_id.city.$photo->city_id", $incrXp, $userId);
+
+        $year = now()->year;
+        $month = now()->month;
+        $day = now()->day;
+
+        Redis::zincrby("leaderboards.country.$photo->country_id.year.$year", $incrXp, $userId);
+        Redis::zincrby("leaderboards.country.$photo->country_id.year.$year.month.$month", $incrXp, $userId);
+        Redis::zincrby("leaderboards.country.$photo->country_id.year.$year.month.$month.day.$day", $incrXp, $userId);
     }
 }
