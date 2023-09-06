59e20eebb82308ff1db377be538ddfd0177ce5c1
xlcrr <info@openlittermap.com>
2023-09-06 18:13:22 +0100

    update iOS version 5.1.4

--- app/Http/Controllers/API/MobileAppVersionController.php
+++ app/Http/Controllers/API/MobileAppVersionController.php
@@ -14,7 +14,7 @@ class MobileAppVersionController extends Controller
         return [
             'ios' => [
                 'url' => 'https://apps.apple.com/us/app/openlittermap/id1475982147',
-                'version' => '5.1.0'
+                'version' => '5.1.4'
             ],
             'android' => [
                 'url' => 'https://play.google.com/store/apps/details?id=com.geotech.openlittermap',
