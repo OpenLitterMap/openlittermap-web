b3f7087acddd14a017ec2cd546f5a1a2fe059ca8
xlcrr <info@openlittermap.com>
2024-06-29 12:42:24 +0100

    here we go

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,9 +24,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
-        Passport::routes();
+        // Passport::routes();
         Passport::tokensExpireIn(now()->addDays(365));
         Passport::refreshTokensExpireIn(now()->addDays(365));
 
