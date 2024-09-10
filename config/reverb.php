a29548ffabd03a15332ebc13320e9effadad9db7
xlcrr <info@openlittermap.com>
2024-09-10 11:24:58 +0100

    compile to test websockets

--- config/reverb.php
+++ config/reverb.php
@@ -27,7 +27,6 @@ return [
     */
 
     'servers' => [
-
         'reverb' => [
             'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
             'port' => env('REVERB_SERVER_PORT', 8080),
@@ -51,7 +50,6 @@ return [
             'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
             'telescope_ingest_interval' => env('REVERB_TELESCOPE_INGEST_INTERVAL', 15),
         ],
-
     ],
 
     /*
