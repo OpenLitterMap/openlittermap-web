0c72d712ced42d6df03119566f0c99237ef2f05b
xlcrr <info@openlittermap.com>
2024-09-24 16:31:43 +0100

    get total_litter

--- app/Http/Controllers/Reports/GenerateImpactReportController.php
+++ app/Http/Controllers/Reports/GenerateImpactReportController.php
@@ -48,7 +48,7 @@ class GenerateImpactReportController extends Controller
 
             // Tags
             // We should move this to Redis
-            $totalTags = Photo::sum('tags_count');
+            $totalTags = Photo::sum('total_litter');
 
             [$topTags, $topBrands, $newTags] = $this->getTopLitter($start, $end);
 
