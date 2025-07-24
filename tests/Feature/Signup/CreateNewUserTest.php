91b449666212165defb875f9225c9f3f5a54dd54
Seán L <lynch.sean@pm.me>
2025-07-24 13:39:08 +0100

    update password test

--- tests/Feature/Signup/CreateNewUserTest.php
+++ tests/Feature/Signup/CreateNewUserTest.php
@@ -12,54 +12,54 @@ class CreateNewUserTest extends TestCase
             'name' => 'John Doe',
             'username' => 'username_' . time(),
             'email' => 'test_' . time() . '@example.com',
-            'password' => 'ReallyStrongPassword123!',
+            'password' => 'password!',
             'password_confirmation' => 'password',
         ]);
 
         $this->assertEquals(200, $response->getStatusCode());
     }
 
-    public static function passwordProvider (): array
-    {
-        return [
-            'missing_uppercase' => [
-                'password' => 'lowercase1#',
-                'error' => 'The password must contain at least one uppercase and one lowercase letter.'
-            ],
-            'missing_lowercase' => [
-                'password' => 'UPPERCASE1#',
-                'error' => 'The password must contain at least one uppercase and one lowercase letter.'
-            ],
-            'missing_numbers' => [
-                'password' => 'UpperLower#',
-                'error' => 'The password must contain at least one number.'
-            ],
-            'missing_symbols' => [
-                'password' => 'UpperLower1',
-                'error' => 'The password must contain at least one special character.'
-            ],
-        ];
-    }
+//    public static function passwordProvider (): array
+//    {
+//        return [
+//            'missing_uppercase' => [
+//                'password' => 'lowercase1#',
+//                'error' => 'The password must contain at least one uppercase and one lowercase letter.'
+//            ],
+//            'missing_lowercase' => [
+//                'password' => 'UPPERCASE1#',
+//                'error' => 'The password must contain at least one uppercase and one lowercase letter.'
+//            ],
+//            'missing_numbers' => [
+//                'password' => 'UpperLower#',
+//                'error' => 'The password must contain at least one number.'
+//            ],
+//            'missing_symbols' => [
+//                'password' => 'UpperLower1',
+//                'error' => 'The password must contain at least one special character.'
+//            ],
+//        ];
+//    }
 
-    /**
-     * @dataProvider passwordProvider
-     */
-    public function test_a_user_cannot_create_an_account_with_invalid_password ($password, $error)
-    {
-        $response = $this->withoutMiddleware()->post('/register', [
-            'name' => 'John Doe',
-            'username' => 'username_' . time(),
-            'email' => 'test_' . time() . '@example.com',
-            'password' => $password,
-            'password_confirmation' => 'password',
-        ]);
-
-        $this->assertEquals(302, $response->getStatusCode());
-
-        $errors = $response->getSession()->get('errors')->toArray();
-
-        $this->assertArrayHasKey('password', $errors);
-
-        $this->assertTrue(in_array($error, $errors['password']));
-    }
+//    /**
+//     * @dataProvider passwordProvider
+//     */
+//    public function test_a_user_cannot_create_an_account_with_invalid_password ($password, $error)
+//    {
+//        $response = $this->withoutMiddleware()->post('/register', [
+//            'name' => 'John Doe',
+//            'username' => 'username_' . time(),
+//            'email' => 'test_' . time() . '@example.com',
+//            'password' => $password,
+//            'password_confirmation' => 'password',
+//        ]);
+//
+//        $this->assertEquals(302, $response->getStatusCode());
+//
+//        $errors = $response->getSession()->get('errors')->toArray();
+//
+//        $this->assertArrayHasKey('password', $errors);
+//
+//        $this->assertTrue(in_array($error, $errors['password']));
+//    }
 }
